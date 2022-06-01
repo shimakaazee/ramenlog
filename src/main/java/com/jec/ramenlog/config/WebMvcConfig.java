42ab8991383aadc34c3da949b5487ca40c64257c
shimakaazee <shimakaazee@gmail.com>
2022-06-01 17:10:03 +0900

    config file

---
+++ src/main/java/com/jec/ramenlog/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.jec.ramenlog.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected  void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("reloading static resources...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
