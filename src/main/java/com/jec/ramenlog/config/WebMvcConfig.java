151c0b330b7d8d7d3af5a8141b7aef09aa0dacb8
jellyman <jellyman2012@gmail.com>
2022-11-14 19:38:35 +0900

    update

--- src/main/java/com/jec/ramenlog/config/WebMvcConfig.java
+++ src/main/java/com/jec/ramenlog/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.jec.ramenlog.config;
 
+import com.jec.ramenlog.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -14,5 +19,12 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("reloading static resources...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        converters.add(0,messageConverter);
     }
 }
